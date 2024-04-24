230220548a6b0d08b044977113885586090456e8
Bryan Rizqi Prakosa <bryanrp1950@gmail.com>
2024-04-24 08:17:14 +0700

    feat : CI

--- src/Commons/config.js
+++ src/Commons/config.js
@@ -1,31 +1,43 @@
 /* istanbul ignore file */
-const dotenv = require('dotenv')
-const path = require('path')
+let config
 
 if (process.env.NODE_ENV === 'test') {
-  dotenv.config({
-    path: path.resolve(process.cwd(), '.test.env')
-  })
+  config = {
+    app: {
+      host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+      port: process.env.PORT_TEST
+    },
+    database: {
+      host: process.env.PGHOST,
+      port: process.env.PGPORT,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE_TEST
+    },
+    jwt: {
+      accessTokenKey: process.env.ACCESS_TOKEN_KEY,
+      accessTokenAge: process.env.ACCESS_TOKEN_AGE,
+      refreshTokenKey: process.env.REFRESH_TOKEN_KEY
+    }
+  }
 } else {
-  dotenv.config()
-}
-
-const config = {
-  app: {
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-    port: process.env.PORT
-  },
-  database: {
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE
-  },
-  jwt: {
-    accessTokenKey: process.env.ACCESS_TOKEN_KEY,
-    accessTokenAge: process.env.ACCESS_TOKEN_AGE,
-    refreshTokenKey: process.env.REFRESH_TOKEN_KEY
+  config = {
+    app: {
+      host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+      port: process.env.PORT
+    },
+    database: {
+      host: process.env.PGHOST,
+      port: process.env.PGPORT,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE
+    },
+    jwt: {
+      accessTokenKey: process.env.ACCESS_TOKEN_KEY,
+      accessTokenAge: process.env.ACCESS_TOKEN_AGE,
+      refreshTokenKey: process.env.REFRESH_TOKEN_KEY
+    }
   }
 }
 
